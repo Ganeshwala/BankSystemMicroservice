e98987c4c750ca0e1fba30c513ccc7f77fc2bdfc
ganeshwala <ganeshwalachintan@gmail.com>
2024-10-21 19:45:33 -0400

    Interaction with H2 DB

--- Loans/src/main/java/com/bank/loans/services/Impl/LoansServiceImpl.java
+++ Loans/src/main/java/com/bank/loans/services/Impl/LoansServiceImpl.java
@@ -1,9 +1,17 @@
 package com.bank.loans.services.Impl;
 
+import java.util.Optional;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bank.loans.constants.LoansConstants;
+import com.bank.loans.dto.LoansDto;
 import com.bank.loans.entities.Loans;
+import com.bank.loans.exception.LoanAlreadyExistsException;
+import com.bank.loans.exception.ResourceNotFoundException;
+import com.bank.loans.mapper.LoansMapper;
 import com.bank.loans.repository.LoansRepository;
 import com.bank.loans.services.LoansService;
 
@@ -11,30 +19,57 @@ import com.bank.loans.services.LoansService;
 public class LoansServiceImpl implements LoansService{
 
 	@Autowired
-	LoansRepository loanRepository;
+	LoansRepository loansRepository;
 	
 	@Override
 	public void createLoan(String mobileNumber) {
-		// TODO Auto-generated method stub
-		
+		Optional<Loans> optionalLoans= loansRepository.findByMobileNumber(mobileNumber);
+        if(optionalLoans.isPresent()){
+            throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber "+mobileNumber);
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
 	}
-
-	@Override
-	public boolean updateLoanDeatil(Loans laonInfo) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	
+	/**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new loan details
+     */
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
 
 	@Override
 	public boolean deleteLoan(String mobileNumber) {
-		// TODO Auto-generated method stub
-		return false;
+		Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        loansRepository.deleteById(loans.getLoanId());
+        return true;
 	}
 
 	@Override
-	public Loans fetchLoan(String mobileNumber) {
-		// TODO Auto-generated method stub
-		return null;
+	public LoansDto fetchLoan(String mobileNumber) {
+		Loans loans = loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+        );
+        return LoansMapper.mappedLoansToLoansDto(loans, new LoansDto());
+	}
+
+	@Override
+	public boolean updateLoan(LoansDto loansDto) {
+		Loans loans = loansRepository.findByLoanNumber(loansDto.getLoanNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Loan", "LoanNumber", loansDto.getLoanNumber()));
+        LoansMapper.mappedLoansDtoToLoans(loansDto, loans);
+        loansRepository.save(loans);
+        return  true;
 	}
 
 }
