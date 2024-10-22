53573138048dddc98707990603e11c7535564653
ganeshwala <ganeshwalachintan@gmail.com>
2024-10-21 20:42:19 -0400

    Modify Card Module Code

--- Cards/src/main/java/com/bank/cards/audit/AuditAwareImpl.java
+++ Cards/src/main/java/com/bank/cards/audit/AuditAwareImpl.java
@@ -3,7 +3,9 @@ package com.bank.cards.audit;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
+@Component(value ="auditAwareImpl" )
 public class AuditAwareImpl implements AuditorAware<String>{
 	 @Override
 	    public Optional<String> getCurrentAuditor() {
